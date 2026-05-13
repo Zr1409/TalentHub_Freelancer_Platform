5de8c036df5834fad1f7a7e8f4934d8824d8ced4
dutv <tranvandu19971029@gmail.com>
2026-05-13 13:50:20 +0700

    feat: add enums classes

---
+++ TalentHub-backend/src/main/java/com/backend/enums/ErrorCode.java
@@ -0,0 +1,22 @@
+
+package com.backend.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    INVALID_TOKEN(1001, "Invalid Token")
+    ;
+
+    private final Integer status;
+    private final String message;
+
+    // Constructor yêu cầu tham số
+    ErrorCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
---
+++ TalentHub-backend/src/main/java/com/backend/enums/ReportedJobStatus.java
@@ -0,0 +1,7 @@
+package com.backend.enums;
+
+public enum ReportedJobStatus {
+//    REPORTED,
+    RESOLVED,
+    IN_PROGRESS
+}
